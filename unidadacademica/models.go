6b0c004bfcdf74125fc7daf5786e2045be40e05c
HectorLiAd <hectorlimahuaya@upeu.edu.pe>
2020-11-30 22:08:11 -0500

    HHHh

--- unidadacademica/models.go
+++ unidadacademica/models.go
@@ -1,11 +1,9 @@
 package unidadacademica
 
-import "github.com/API_REST_BDII_LP2_MYSQL/tipounidad"
-
 /*UnidadAcademica obtener la unidad academica*/
 type UnidadAcademica struct {
-	ID           int                   `json:"id"`
-	TipoUnidadID int                   `json:"idTipoUnidad"`
-	Nombre       string                `json:"nombre"`
+	ID           int    `json:"id"`
+	TipoUnidadID int    `json:"idTipoUnidad"`
+	Nombre       string `json:"nombre"`
 	// TipoUnidad   tipounidad.TipoUnidad `json:"TipoUnidad,omitempty"`
 }
--- verionesrouter/router.go
+++ verionesrouter/router.go
@@ -8,6 +8,7 @@ import (
 	"github.com/API_REST_BDII_LP2_MYSQL/middlew"
 	"github.com/API_REST_BDII_LP2_MYSQL/persona"
 	"github.com/API_REST_BDII_LP2_MYSQL/tipounidad"
+	"github.com/API_REST_BDII_LP2_MYSQL/unidadacademica"
 	"github.com/API_REST_BDII_LP2_MYSQL/usuario"
 	"github.com/API_REST_BDII_LP2_MYSQL/usuariologin"
 	"github.com/go-chi/chi"
@@ -18,25 +19,24 @@ func RouterV1(db *sql.DB) http.Handler {
 	r := chi.NewRouter()
 	r.Use(helper.GetCors().Handler)
 	var (
-		usuarioRepository      = usuario.NewRepository(db)
-		personaRepository      = persona.NewRepository(db)
-		usuarioLoginRepository = usuariologin.NewRepository(db)
-		tipoUnidadRepository   = tipounidad.NewRepository(db)
-		// unidadAcademicaRepository = unidadacademica.NewRepository(db)
+		usuarioRepository         = usuario.NewRepository(db)
+		personaRepository         = persona.NewRepository(db)
+		usuarioLoginRepository    = usuariologin.NewRepository(db)
+		tipoUnidadRepository      = tipounidad.NewRepository(db)
+		unidadAcademicaRepository = unidadacademica.NewRepository(db)
 	)
 	var (
-		usuarioService      = usuario.NewService(usuarioRepository)
-		personaService      = persona.NerService(personaRepository)
-		usuarioLoginService = usuariologin.NewService(usuarioLoginRepository)
-		tipoUnidadService   = tipounidad.NewService(tipoUnidadRepository)
-		// unidadAcademicaService = unidadacademica.NewService(unidadAcademicaRepository)
+		usuarioService         = usuario.NewService(usuarioRepository)
+		personaService         = persona.NerService(personaRepository)
+		usuarioLoginService    = usuariologin.NewService(usuarioLoginRepository)
+		tipoUnidadService      = tipounidad.NewService(tipoUnidadRepository)
+		unidadAcademicaService = unidadacademica.NewService(unidadAcademicaRepository)
 	)
-
 	r.Mount("/usuario", middlew.ValidoJWT(usuario.MakeHTTPSHandler(usuarioService)))
 	r.Mount("/usuariologin", usuariologin.MakeHTTPSHandler(usuarioLoginService))
 	r.Mount("/persona", persona.MakeHTTPSHandler(personaService))
 	r.Mount("/tipoUnidad", tipounidad.MakeHTTPSHandler(tipoUnidadService))
-	// r.Mount("/unidadAcademica", unidadacademica.MakeHTTPSHandler(unidadAcademicaService))
+	r.Mount("/unidadAcademica", unidadacademica.MakeHTTPSHandler(unidadAcademicaService))
 
 	return r
 }
