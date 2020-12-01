435a96780c6122d8bd80e765a898dbd9547ee537
HectorLiAd <hectorlimahuaya@upeu.edu.pe>
2020-11-30 21:49:52 -0500

    2

--- main.go
+++ main.go
@@ -5,15 +5,8 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/API_REST_BDII_LP2_MYSQL/middlew"
-	"github.com/API_REST_BDII_LP2_MYSQL/unidadacademica"
-
 	"github.com/API_REST_BDII_LP2_MYSQL/database"
-	"github.com/API_REST_BDII_LP2_MYSQL/helper"
-	"github.com/API_REST_BDII_LP2_MYSQL/persona"
-	"github.com/API_REST_BDII_LP2_MYSQL/tipounidad"
-	"github.com/API_REST_BDII_LP2_MYSQL/usuario"
-	"github.com/API_REST_BDII_LP2_MYSQL/usuariologin"
+	"github.com/API_REST_BDII_LP2_MYSQL/verionesrouter"
 	"github.com/go-chi/chi"
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -22,30 +15,8 @@ func main() {
 	db := database.InitDB()
 	fmt.Println(db)
 	defer db.Close()
-
 	r := chi.NewRouter()
-	r.Use(helper.GetCors().Handler)
-	var (
-		usuarioRepository         = usuario.NewRepository(db)
-		personaRepository         = persona.NewRepository(db)
-		usuarioLoginRepository    = usuariologin.NewRepository(db)
-		tipoUnidadRepository      = tipounidad.NewRepository(db)
-		unidadAcademicaRepository = unidadacademica.NewRepository(db)
-	)
-	var (
-		usuarioService         = usuario.NewService(usuarioRepository)
-		personaService         = persona.NerService(personaRepository)
-		usuarioLoginService    = usuariologin.NewService(usuarioLoginRepository)
-		tipoUnidadService      = tipounidad.NewService(tipoUnidadRepository)
-		unidadAcademicaService = unidadacademica.NewService(unidadAcademicaRepository)
-	)
-
-	r.Mount("/usuario", middlew.ValidoJWT(usuario.MakeHTTPSHandler(usuarioService)))
-	r.Mount("/usuariologin", usuariologin.MakeHTTPSHandler(usuarioLoginService))
-	r.Mount("/persona", persona.MakeHTTPSHandler(personaService))
-	r.Mount("/tipoUnidad", tipounidad.MakeHTTPSHandler(tipoUnidadService))
-	r.Mount("/unidadAcademica", unidadacademica.MakeHTTPSHandler(unidadAcademicaService))
-
+	r.Mount("/v1", verionesrouter.Router(db))
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
--- routers/routers.go
+++ routers/routers.go
@@ -1,9 +1,1 @@
 package routers
-
-// func routerProy(db *sql.DB) http.Handler {
-// 	r := chi.NewRouter()
-
-// 	r.Mo
-
-// 	r
-// }
