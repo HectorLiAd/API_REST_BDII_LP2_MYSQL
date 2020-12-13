40d955c01b3b18ad2720106f6f93cd784fcde097
HectorLiAd <hectorlimahuaya@upeu.edu.pe>
2020-12-13 10:09:31 -0500

    Refactor Uusiaro login

--- tables/usuariologin/reqResponse.go
+++ tables/usuariologin/reqResponse.go
@@ -3,7 +3,6 @@ package usuariologin
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"time"
 	// "time"
@@ -12,11 +11,9 @@ import (
 /*EncodeJSONResponseFileImgUpload sirve para poderlo usar de forma general*/
 func encodeJSONResponseLogin(_ context.Context, w http.ResponseWriter, request interface{}) error {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-
 	//Subiendo el token a la Cookie
-	expirationTime := time.Now().Add(24 * time.Hour) //
+	expirationTime := time.Now().Add(time.Hour * 24) //
 	cookie := http.Cookie{Name: "token", Value: request.(*RespuestaLogin).Token, Expires: expirationTime}
 	http.SetCookie(w, &cookie)
-	fmt.Println(request.(*RespuestaLogin).Token)
 	return json.NewEncoder(w).Encode(request)
 }
